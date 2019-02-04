b0b996e83c0f3e13301d7063016c0d4e4d662243
Atan1988 <tzw0423@hotmail.com>
2019-02-03 20:16:45 -0500

    commit

--- inst/RL with reticulate.R
+++ inst/RL with reticulate.R
@@ -1,7 +1,8 @@
 library(reticulate)
 
-# use_python('C:/Program Files/Python/Python36', required = T)
-# use_virtualenv('c:/venv', required = TRUE)
+#use_python('C:/Program Files/Python/Python36', required = T)
+#use_virtualenv('c:/venv', required = TRUE)
+#use_condaenv('C:/ProgramData/Anaconda3', required = TRUE)
 difflib <- import("difflib")
 
 main <- import_main()
@@ -13,3 +14,5 @@ source_python('inst/add.py')
 add(5, 10)
 
 tf <- import('tensorflow')
+np <- import('numpy')
+keras <- import('keras')
